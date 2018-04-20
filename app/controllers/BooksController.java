f19b485be2f7f97340757dd5f773068a46d7b19b
robuntu <robuntu@robuntu.ca>
2018-04-20 12:26:40 -0400

    Finish videos 16-19

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -31,24 +31,49 @@ public class BooksController extends Controller {
 
     // to save book
     public Result save(){
-
-        return TODO;
+        Form<Book> bookForm = formFactory.form(Book.class).bindFromRequest();
+        Book book = bookForm.get();
+        Book.add(book);
+        return redirect(routes.BooksController.index());
     }
 
     public Result edit(Integer id){
-        return TODO;
+        Book book = Book.findById(id);
+        if (book==null){
+            return notFound("Book Not Found");
+        }
+        Form<Book> bookForm = formFactory.form(Book.class).fill(book);
+        return ok(edit.render(bookForm));
     }
 
     public Result update(){
-        return TODO;
+        Book book = formFactory.form(Book.class).bindFromRequest().get();
+        Book oldBook = Book.findById(book.getId());
+        if(oldBook==null){
+            return notFound("Book Not Found");
+        }
+        oldBook.setTitle(book.getTitle());
+        oldBook.setAuthor(book.getAuthor());
+        oldBook.setPrice(book.getPrice());
+
+        return redirect(routes.BooksController.index());
     }
 
     public Result destroy(Integer id){
-        return TODO;
+        Book book = Book.findById(id);
+        if (book == null){
+            return notFound("Book not found");
+        }
+        Book.remove(book);
+        return redirect(routes.BooksController.index());
     }
 
     public Result show(Integer id){
-        return TODO;
+        Book book = Book.findById(id);
+        if (book==null){
+            return notFound("Book not found");
+        }
+        return ok(show.render(book));
     }
 
 }
